f8ea528cecf6cd24b7d63ce6b158690a9fea06d3
justdevthat <justdevthat@gmail.com>
2024-01-02 17:52:51 +0300

    Initial commit

---
+++ src/main/java/ru/gmdev/comfy_doc/ComfyDocApplication.java
@@ -0,0 +1,14 @@
+package ru.gmdev.comfy_doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComfyDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComfyDocApplication.class, args);
+		System.out.println("Running my app");
+	}
+
+}
---
+++ src/test/java/ru/gmdev/comfy_doc/ComfyDocApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gmdev.comfy_doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComfyDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
